9d29e484b37fce9b9fea3e94c5a0059e03aca3a9
Hwang Dayoung <hdangdang00@gmail.com>
2025-12-01 19:57:29 +0900

    Time: 0 ms (100%), Space: 58.4 MB (26.95%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let ans = []
-    let len = nums.length
-    for (i = 0; i < len; i++) {
-        ans[i] = nums[i]
-        ans[i + len] = nums[i]
-    }
-    return ans
+    return nums.concat(nums)
 }
