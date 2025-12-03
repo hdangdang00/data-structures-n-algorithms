2b42331c12c59a5824179f585f18793664892eb5
Hwang Dayoung <hdangdang00@gmail.com>
2025-12-03 19:37:15 +0900

    Time: 6 ms (75.61%), Space: 57.8 MB (86.03%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -3,12 +3,20 @@
  * @return {number[]}
  */
 var findErrorNums = function(nums) {
-    let repetitionNum = 0
-    let lossNum = 0
-    for (let i = 0; i < nums.length; i++) {
-        const _nums = nums.filter(num => num === i + 1)
-        if (_nums.length > 1) repetitionNum = i + 1
-        else if (_nums.length === 0) lossNum = i + 1
+    let n = nums.length
+    let count = Array(n).fill(0)
+
+    let duplicate = 0
+    let missing = 0
+
+    for (let num of nums) {
+        count[num - 1]++
     }
-    return [repetitionNum, lossNum]
+
+    for (let i = 0; i < n; i++) {
+        if (count[i] > 1) duplicate = i + 1
+        else if (count[i] === 0) missing = i + 1
+    }
+
+    return [duplicate, missing]
 }
